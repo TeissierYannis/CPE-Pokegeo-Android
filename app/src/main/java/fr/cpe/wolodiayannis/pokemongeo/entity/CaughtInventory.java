0472e148bdfc879ce4ce6f3dc6936e5b16b63002
Wolodia Zdetovetzky <wolozdeto@gmail.com>
2022-09-10 18:46:47 +0200

    Caught Inventory TU

--- app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/entity/CaughtInventory.java
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/entity/CaughtInventory.java
@@ -4,22 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CaughtInventory {
-    private final int MAX_CAUGHT = 1000;
     private final List<Pokemon> caughtPokemonInventory;
 
     public CaughtInventory() {
         this.caughtPokemonInventory = new ArrayList<>();
     }
 
+
     public Pokemon getCaughtPokemon(int index) {
-        if (index < 0 || index >= MAX_CAUGHT) {
+        if (index < 0 || index >= this.caughtPokemonInventory.size()) {
             throw new InternalError("The index is out of the caught inventory");
         } else {
-            if (index < caughtPokemonInventory.size()) {
-                return caughtPokemonInventory.get(index);
-            } else {
-                return null;
-            }
+            return caughtPokemonInventory.get(index);
         }
     }
 
@@ -31,10 +27,8 @@ public class CaughtInventory {
         if (this.pokemonIsCaught(pokemon)) {
             this.caughtPokemonInventory.get(this.caughtPokemonInventory.indexOf(pokemon)).addQuantity(quantity);
         } else {
-            if (!this.caughtPokemonInventoryIsFull()) {
-                pokemon.addQuantity(quantity);
-                this.caughtPokemonInventory.add(pokemon);
-            }
+            pokemon.addQuantity(quantity);
+            this.caughtPokemonInventory.add(pokemon);
         }
         return this;
     }
@@ -58,13 +52,6 @@ public class CaughtInventory {
         throw new RuntimeException("The pokemon wasn't caught");
     }
 
-    private boolean caughtPokemonInventoryIsFull() {
-        if (this.caughtPokemonInventory.size() >= MAX_CAUGHT) {
-            throw new RuntimeException("The caught inventory is full");
-        } else {
-            return false;
-        }
-    }
 
     private boolean pokemonIsCaught(Pokemon pokemon) {
         for (Pokemon i : caughtPokemonInventory) {
