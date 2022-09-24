009b9006c453f9bfbbe3752a9800182fcb98b494
Teissier Yannis <yannis.teissier70@gmail.com>
2022-09-24 08:02:20 +0200

    Start adding pokemon on the map

--- app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/fragments/MapFragment.java
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/fragments/MapFragment.java
@@ -1,5 +1,7 @@
 package fr.cpe.wolodiayannis.pokemongeo.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
@@ -22,12 +25,14 @@ import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import fr.cpe.wolodiayannis.pokemongeo.MainActivity;
 import fr.cpe.wolodiayannis.pokemongeo.R;
 import fr.cpe.wolodiayannis.pokemongeo.databinding.MapFragmentBinding;
 import fr.cpe.wolodiayannis.pokemongeo.entity.Pokemon;
+import fr.cpe.wolodiayannis.pokemongeo.utils.Logger;
 
 /**
  * MapFragment.
@@ -49,10 +54,13 @@ public class MapFragment extends Fragment {
 
     GeoPoint actualPosition;
 
+    Date lastUpdate;
+
     /**
      * onCreateView.
-     * @param inflater inflater
-     * @param container container
+     *
+     * @param inflater           inflater
+     * @param container          container
      * @param savedInstanceState savedInstanceState
      * @return view
      */
@@ -100,6 +108,7 @@ public class MapFragment extends Fragment {
 
     /**
      * Update geo point.
+     *
      * @param mainActivity main activity
      */
     public void getMapAsync(MainActivity mainActivity) {
@@ -109,6 +118,8 @@ public class MapFragment extends Fragment {
         map.getController().setCenter(this.actualPosition);
         // Ask for recenter the screen
         map.invalidate();
+
+        generatePokemonOnMap();
     }
 
     /**
@@ -118,30 +129,62 @@ public class MapFragment extends Fragment {
      * - a timer (5minutes)
      */
     public void generatePokemonOnMap() {
-        // random between 3 and 10
-        int nbPokemon = (int) (Math.random() * 7) + 3;
-        // list of nb pokemon
-        int totalPokemon = MainActivity.getDataList().getPokemons().size();
-        // Generate nb pokemon
-        for (int i = 0; i < nbPokemon; i++) {
-            // random pokemon
-            int randomPokemon = (int) (Math.random() * totalPokemon);
 
-            // random position in a 100m radius
-            double randomLat = (Math.random() * 0.0009) + 0.0001;
-            double randomLon = (Math.random() * 0.0009) + 0.0001;
-            // random position
-            double lat = this.actualPosition.getLatitude() + randomLat;
-            double lon = this.actualPosition.getLongitude() + randomLon;
+        // if the last update is more than 5 minutes
+        if (this.lastUpdate == null || (new Date().getTime() - this.lastUpdate.getTime()) > 300000) {
+            this.lastUpdate = new Date();
 
-            // Create marker with pokemon
-            //Marker marker = new Marker(map);
-            //marker.setPosition(new GeoPoint(lat, lon));
-            //marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            //marker.setIcon(MainActivity.getDataList().getPokemons().get(randomPokemon).getImageID());
+            // random between 3 and 10
+            int nbPokemon = (int) (Math.random() * 7) + 3;
+            // list of nb pokemon
+            int totalPokemon = MainActivity.getDataList().getPokemons().size();
+            // Generate nb pokemon
+            for (int i = 0; i < nbPokemon; i++) {
+                // random pokemon
+                int randomPokemon = (int) (Math.random() * totalPokemon);
+                Pokemon pokemon = MainActivity.getDataList().getPokemons().get(randomPokemon);
+                // random position in a 100m radius
+                double randomLat = (Math.random() * 0.0009) + 0.0001;
+                double randomLon = (Math.random() * 0.0009) + 0.0001;
+                // random position
+                double lat = this.actualPosition.getLatitude() + randomLat;
+                double lon = this.actualPosition.getLongitude() + randomLon;
+
+                // Create marker with pokemon
+                Marker marker = new Marker(map);
+                marker.setPosition(new GeoPoint(lat, lon));
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                // From image ID get drawable with compat
+
+                Drawable img = AppCompatResources.getDrawable(
+                        requireContext(),
+                        pokemon.getImageID()
+                );
+                // set drawable size
+                //assert img != null;
+                //img.setBounds(0, 0, 100, 100);
+
+                try {
+                    marker.setIcon(
+                            img
+                    );
+                    map.getOverlays().add(marker);
+                    // set size of icon
+                    marker.setInfoWindow(null);
+                    marker.setRelatedObject(pokemon);
+                    marker.setOnMarkerClickListener((marker1, mapView) -> {
+                        Pokemon pokemon1 = (Pokemon) marker1.getRelatedObject();
+                        Logger.log("Pokemon clicked : " + pokemon1.getName());
+                        return false;
+                    });
+
+                } catch (Exception e) {
+                    Logger.logOnUiThreadError("[DRAWABLE] id = " + pokemon.getImageID(), e);
+                }
+
+
+            }
         }
-
-
     }
 
 
