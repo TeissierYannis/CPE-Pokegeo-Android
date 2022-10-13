736af0041e669c94435f78958a578a1dc013d844
Teissier Yannis <yannis.teissier70@gmail.com>
2022-10-13 17:35:57 +0200

    init user profile

---
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/fragments/UserProfileFragment.java
@@ -0,0 +1,54 @@
+package fr.cpe.wolodiayannis.pokemongeo.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.Objects;
+
+import fr.cpe.wolodiayannis.pokemongeo.R;
+import fr.cpe.wolodiayannis.pokemongeo.data.Datastore;
+import fr.cpe.wolodiayannis.pokemongeo.databinding.UserProfileBinding;
+import fr.cpe.wolodiayannis.pokemongeo.viewmodel.UserViewModel;
+;
+
+public class UserProfileFragment extends Fragment {
+
+    /**
+     * onCreateView.
+     *
+     * @param inflater           inflater
+     * @param container          container
+     * @param savedInstanceState savedInstanceState
+     * @return view
+     */
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        UserProfileBinding binding = DataBindingUtil.inflate(inflater, R.layout.user_profile, container, false);
+        // Set the layout
+        UserViewModel userViewModel = new UserViewModel();
+
+        userViewModel.setUser(Datastore.getInstance().getUser());
+        userViewModel.setPokedexCount(Datastore.getInstance().getCaughtInventory().getCaughtInventoryList().size() + "/" + Datastore.getInstance().getPokemons().size());
+
+        binding.setUserViewModel(userViewModel);
+
+        binding.chevronBack.setOnClickListener(v -> {
+            requireActivity().onBackPressed();
+        });
+
+        return binding.getRoot();
+    }
+
+}
---
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/viewmodel/UserViewModel.java
@@ -0,0 +1,55 @@
+package fr.cpe.wolodiayannis.pokemongeo.viewmodel;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.res.ResourcesCompat;
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import fr.cpe.wolodiayannis.pokemongeo.data.Datastore;
+import fr.cpe.wolodiayannis.pokemongeo.entity.item.Item;
+import fr.cpe.wolodiayannis.pokemongeo.entity.user.User;
+
+/**
+ * Item View Model.
+ */
+public class UserViewModel extends BaseObservable {
+
+    private User user;
+    private String pokedexCount;
+
+    /**
+     * Set the item.
+     *
+     * @param user the item
+     */
+    public void setUser(User user) {
+        this.user = user;
+        notifyChange();
+    }
+
+    public void setPokedexCount(String count) {
+        this.pokedexCount = count;
+        notifyChange();
+    }
+
+    @Bindable
+    public String getUserPseudo() {
+        return user.getPseudo().toUpperCase();
+    }
+    @Bindable
+    public String getUserEmail() {
+        return user.getEmail();
+    }
+
+    @Bindable
+    public String getUserMoney() {
+        return String.valueOf(user.getMoney());
+    }
+
+    @Bindable
+    public String getPokedexCount() {
+        return pokedexCount;
+    }
+}
