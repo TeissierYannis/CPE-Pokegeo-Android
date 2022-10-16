d688a1ffeaf8ae50b5364249483354c8f07c0f78
Teissier Yannis <yannis.teissier70@gmail.com>
2022-10-16 10:41:55 +0200

    fix buttons

--- app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/fragments/UserProfileFragment.java
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/fragments/UserProfileFragment.java
@@ -2,10 +2,12 @@ package fr.cpe.wolodiayannis.pokemongeo.fragments;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -216,7 +218,6 @@ public class UserProfileFragment extends Fragment {
         });
 
         this.binding.friendRequestButton.setOnClickListener(v -> {
-
             binding.friendsRequestCount.setText("loading...");
 
             FriendRequestListAdapter adapter = new FriendRequestListAdapter();
