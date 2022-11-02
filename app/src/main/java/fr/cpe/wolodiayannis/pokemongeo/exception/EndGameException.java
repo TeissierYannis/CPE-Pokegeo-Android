cca95c710f243d769ff643d46522d851bf69178a
Wolodia Zdetovetzky <wolozdeto@gmail.com>
2022-11-02 14:02:14 +0100

    add EndGameException and fix friends colors

---
+++ app/src/main/java/fr/cpe/wolodiayannis/pokemongeo/exception/EndGameException.java
@@ -0,0 +1,13 @@
+package fr.cpe.wolodiayannis.pokemongeo.exception;
+
+public class EndGameException extends Exception {
+
+    /**
+     * Constructor.
+     *
+     * @param message message
+     */
+    public EndGameException(String message) {
+        super(message);
+    }
+}
